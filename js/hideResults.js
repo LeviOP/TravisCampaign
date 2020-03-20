2f403ac769a5ccc1cc4ef93aedc1938bac04e453
LeviOP <35459268+LeviOP@users.noreply.github.com>
2020-03-20 10:15:15 -0500

    Update hideResults.js

--- js/hideResults.js
+++ js/hideResults.js
@@ -1,1 +1,5 @@
-
+window.addEventListener("onload", function(){
+  document.querySelectorAll("div")[21].children[1].remove()
+  document.querySelectorAll("div")[21].children[1].remove()
+  document.querySelectorAll("div")[21].children[1].remove()
+})
